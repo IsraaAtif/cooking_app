2b3e99ef2a34fecffa12abba9db54b2945df8c1d
IsraaAtif <Israaatif333@gmail.com>
2026-09-06 22:11:13 +0300

    Initial commit

---
+++ lib/cooking_app.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class CookingApp extends StatelessWidget {
+  const CookingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     home: ,   
+    );
+  }
+}
